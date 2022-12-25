9c638e718efd6d301fd009b86b1d4afc279295bb
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-25 15:54:57 +0100

    form contact style

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,26 +3,32 @@ import React from 'react';
 const ContactForm = () => {
 	return (
 		<div className="form-container">
-			<h2>Me contacter</h2>
 			<form className="form-content">
-				<label>Nom</label>
-				<input
-					type="text"
-					name="name"
-					required
-					autoComplete="off"
-					id="name"
-				/>
-				<label>Email</label>
-				<input
-					type="email"
-					name="email"
-					required
-					autoComplete="off"
-					id="email"
-				/>
-				<label>Message</label>
-				<textarea name="message" id="mess" />
+				<h2>Me contacter</h2>
+				<div className="name-container">
+					<label>Nom</label>
+					<input
+						type="text"
+						name="name"
+						required
+						autoComplete="off"
+						id="name"
+					/>
+				</div>
+				<div className="email-container">
+					<label>Email</label>
+					<input
+						type="email"
+						name="email"
+						required
+						autoComplete="off"
+						id="email"
+					/>
+				</div>
+				<div className="message-container">
+					<label>Message</label>
+					<textarea name="message" id="mess" />
+				</div>
 				<input type="submit" value="Envoyer" className="hover button" />
 			</form>
 			<div className="formMessage"></div>
