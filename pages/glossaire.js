99cfe9d3821aaab205e20f12a45ac4b73d207b98
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-01-05 17:20:26 +0100

    glossaire expl

--- pages/glossaire.js
+++ pages/glossaire.js
@@ -1,17 +1,40 @@
+import { useState } from 'react';
 import ArrowKey from '../components/ArrowKey';
 import Buttons from '../components/Buttons';
 import Meta from '../components/Meta';
 import { terms } from '../termsData';
 
 const glossaire = () => {
+	const [displayExplanations, setDisplayExplanations] = useState();
+	const [displayExplanations2, setDisplayExplanations2] = useState();
+	const [displayExplanations3, setDisplayExplanations3] = useState();
+
+	const displayTerm = (term) => {
+		setDisplayExplanations(term.explanations);
+		setDisplayExplanations2(term.explanations2);
+		setDisplayExplanations3(term.explanations3);
+	};
 	return (
 		<div className="glossaire">
 			<Meta />
 			<div className="glossaire-container">
 				<h1>Glossaire</h1>
-				{terms.map((term) => (
-					<div key={term.id}>{term.title}</div>
-				))}
+				<div className="glossaire-content">
+					<div className="glossaire-content-title">
+						{terms.map((term) => (
+							<h3 key={term.id} onClick={() => displayTerm(term)}>
+								{term.title}
+							</h3>
+						))}
+					</div>
+					<div className="glossaire-content-explanations">
+						<p>{displayExplanations}</p>
+						<br />
+						<p>{displayExplanations2}</p>
+						<br />
+						<p>{displayExplanations3}</p>
+					</div>
+				</div>
 			</div>
 			<Buttons left={'/contact'} />
 			<ArrowKey />
--- termsData.js
+++ termsData.js
@@ -3,6 +3,8 @@ export const terms = [
 		id: 1,
 		title: 'Développeur',
 		explanations: '',
+		explanations2: '',
+		explanations3: '',
 		link: '',
 		projects: '',
 	},
@@ -10,7 +12,10 @@ export const terms = [
 		id: 2,
 		title: 'Front-End',
 		explanations:
-			"Le Front-End est la partie visible d'un site Web par le public. Il s'agit d'un ensemble de pages Web qui ont été faites par le développeur front-end et qui représentent la partie frontale ou la partie exposée d'un site Web. Les visiteurs peuvent par conséquent visualiser cette partie là sans aucune restriction et peuvent même intéragir avec. Le développeur Front-End doit maitriser les technologies suivantes : <li>HTML: Pour la création du contenu.</li><li>CSS: Pour appliquer le style au contenu HTML intégré.</li><li>JavaScript: Pour mettre en place l'interaction avec l'utilisateur. Des frameworks et librairies JavaScript sont souvent utilisés pour en faciliter le développement comme Angular, React ou Vue.js </li>",
+			"Le Front-End est la partie visible d'un site Web par le public. Il s'agit d'un ensemble de pages Web qui ont été faites par le développeur front-end et qui représentent la partie frontale ou la partie exposée d'un site Web. Les visiteurs peuvent par conséquent visualiser cette partie là sans aucune restriction et peuvent même intéragir avec.",
+		explanations2:
+			"Le développeur Front-End doit maitriser les technologies suivantes : <li>HTML: Pour la création du contenu.</li><li>CSS: Pour appliquer le style au contenu HTML intégré.</li><li>JavaScript: Pour mettre en place l'interaction avec l'utilisateur. Des frameworks et librairies JavaScript sont souvent utilisés pour en faciliter le développement comme Angular, React ou Vue.js </li>",
+		explanations3: `<h1>Wesh</h1>`,
 		link: '',
 		projects: '',
 	},
