b367297342351bb28527669eb26eabf66cf9ce4f
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-02-09 12:06:42 +0100

    contact infos

--- pages/contact.js
+++ pages/contact.js
@@ -4,10 +4,56 @@ import ContactForm from '../components/ContactForm';
 import Meta from '../components/Meta';
 
 const Contact = () => {
+	const copyClipboard = (copyText, textAlert) => {
+		navigator.clipboard.writeText(copyText);
+		alert(textAlert);
+	};
+
 	return (
 		<div className="contact">
 			<Meta />
-			<ContactForm />
+			<div className="contact-container">
+				<ContactForm />
+				<div className="contact-infos">
+					<div className="address">
+						<h4>addresse</h4>
+						<p>54 Clos des Cascades</p>
+						<p>93160 Noisy-le-Grand</p>
+					</div>
+					<div className="phone">
+						<div className="content">
+							<h4>téléphone</h4>
+							<p
+								style={{ cursor: 'pointer' }}
+								className="clipboard"
+								onClick={() =>
+									copyClipboard(
+										'06 42 55 00 71',
+										'Téléphone copié !'
+									)
+								}>
+								06 42 55 00 71
+							</p>
+						</div>
+					</div>
+					<div className="email">
+						<div className="content">
+							<h4>email</h4>
+							<p
+								style={{ cursor: 'pointer' }}
+								className="clipboard"
+								onClick={() =>
+									copyClipboard(
+										'thibault.guilhem@gmail.com',
+										'Email copié !'
+									)
+								}>
+								thibault.guilhem@gmail.com
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 			<Buttons left={'/portfolio'} />
 			<ArrowKey />
 		</div>
