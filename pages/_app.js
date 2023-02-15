8cbabf3d1a868ab25b508dd1c8c66656aa9942c8
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-02-15 11:12:03 +0100

    google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-import Layout from '../components/Layout';
-import '../style/index.scss';
+import Script from "next/script";
+import Layout from "../components/Layout";
+import "../style/index.scss";
 
 export default function App({ Component, pageProps }) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E0TWC6JFDC"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+			window.dataLayer = window.dataLayer || [];
+    		function gtag() {window.dataLayer.push(arguments);}
+			gtag("js", new Date());
+
+    		gtag("config", "G-E0TWC6JFDC");
+			`}
+      </Script>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-E0TWC6JFDC"
-        ></script>
       </Head>
       <body>
         <Main />
