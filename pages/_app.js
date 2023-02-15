dcdcb122101c1723a96af49642d69a4f2a6e5909
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-02-15 11:51:56 +0100

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,12 @@
-import Script from "next/script";
 import Layout from "../components/Layout";
 import "../style/index.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-E0TWC6JFDC"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-			window.dataLayer = window.dataLayer || [];
-    		function gtag() {window.dataLayer.push(arguments);}
-			gtag("js", new Date());
-
-    		gtag("config", "G-E0TWC6JFDC");
-			`}
-      </Script>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
